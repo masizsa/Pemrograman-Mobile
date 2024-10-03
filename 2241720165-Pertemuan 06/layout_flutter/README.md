6fa4da006bffd8cf15842dab3a9d41b53e556437
zalaaaaa <ihza.nk.160903@gmail.com>
2024-10-03 13:18:37 +0700

    [upload]: upload pertemuan 6 bagian praktikum

---
+++ 2241720165-Pertemuan 06/layout_flutter/README.md
@@ -0,0 +1,41 @@
+### Nama : IHza Nurkhafidh Al-Baihaqi
+
+### Kelas : TI 3F
+
+### NIM : 2241720165
+
+<hr>
+<br>
+
+# Praktikum 1: Membangun Layout di FLutter
+
+![alt text](laporan/assets/image.png)
+![alt text](laporan/assets/image-1.png)
+![alt text](laporan/assets/image-2.png)
+![alt text](laporan/assets/image-3.png)
+
+# Praktikum 2: Implementasi Button Row
+
+### Buat method Column \_buildButtonColumn
+
+![alt text](laporan/assets/image-4.png)
+
+### Buat widget buttonSection
+
+![alt text](laporan/assets/image-5.png)
+![alt text](laporan/assets/image-6.png)
+
+# Praktikum 3: Implementasi text section
+
+### Buat widget textSection
+
+![alt text](laporan/assets/image-7.png)
+![alt text](laporan/assets/image-7.png)
+![alt text](laporan/assets/image-8.png)
+
+# Praktikum 4: Implementai laporan/assets/Image Section
+
+![alt text](laporan/assets/image-9.png)
+![alt text](laporan/assets/image-10.png)
+![alt text](laporan/assets/image-11.png)
+![alt text](laporan/assets/image-12.png)
---
+++ 2241720165-Pertemuan 06/layout_flutter/lib/main.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Wisata Sedudo di Nganjuk',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Sawahan, Nganjuk, Indonesia',
+                style: TextStyle(
+                  color: Colors.blueGrey.shade800,
+                ),
+              ),
+            ],
+          ),
+        ),
+        /* soal 3*/
+        const Icon(
+          Icons.star,
+          color: Colors.red,
+        ),
+        const Text('41'),
+      ],
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Air Terjun Sedudo adalah sebuah air terjun dan objek wisata yang '
+        'terletak di Desa Ngliman Kecamatan Sawahan, Kabupaten Nganjuk, Jawa Timur.'
+        'Jaraknya sekitar 30 km arah selatan ibu kota kabupaten Nganjuk. Berada pada '
+        'ketinggian 1.438 meter dpl, ketinggian air terjun ini sekitar 105 meter. \n'
+        'Ihza Nurkhafidh Al-Baihaqi 2241720165',
+        softWrap: true,
+      ),
+    );
+    return MaterialApp(
+        title: 'Flutter Layout: Ihza Nurkhafidh Al-Baihaqi 2241720165',
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Flutter Layout Demo'),
+            ),
+            body: ListView(
+              children: [
+                Image.asset(
+                  'images/sedudo.jpeg',
+                  width: 600,
+                  height: 240,
+                  fit: BoxFit.cover,
+                ),
+                titleSection,
+                buttonSection,
+                textSection
+              ],
+            )));
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
